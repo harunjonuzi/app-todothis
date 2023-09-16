daf7a614db43de7aab794f56041eedac8a3e8ae3
Harun-Mac <iamharunjonuzi@gmail.com>
2023-09-16 17:56:15 +0200

    feat: finished the first version of the todo application, will be looking for better ways of building this on my other branches, and eventually start merging the best solutions slowly and surely.

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,5 @@
-// const listItem = document.querySelectorAll("li");
-
-// function removeChecked() {
-//   listItem.forEach((li) => {
-//     li.classList.remove("checked");
-//   });
-// }
-
-// removeChecked();
-
-// listItem.forEach((li) => {
-//   li.addEventListener("click", function () {
-//     li.classList.toggle("checked");
-//   });
-// });
-
-const inputBox = document.querySelector("input-box");
-const listContainer = document.querySelector("list-container");
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
 
 function addTask() {
   if (inputBox.value === "") {
@@ -24,5 +8,31 @@ function addTask() {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
 }
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName == "SPAN") {
+    // We remove the parent element of span, which is LI itself
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
