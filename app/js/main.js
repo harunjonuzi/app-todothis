e990fb39f31d408194880bbbef9c9a16e502a7e3
Harun-Mac <iamharunjonuzi@gmail.com>
2023-09-16 23:25:13 +0200

    feat: started to add the ui components of the Todoist application

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,38 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+// const inputBox = document.getElementById("input-box");
+// const listContainer = document.getElementById("list-container");
 
-function addTask() {
-  if (inputBox.value === "") {
-    alert("You must write something!");
-  } else {
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-  }
-  inputBox.value = "";
-  saveData();
-}
+// function addTask() {
+//   if (inputBox.value === "") {
+//     alert("You must write something!");
+//   } else {
+//     let li = document.createElement("li");
+//     li.innerHTML = inputBox.value;
+//     listContainer.appendChild(li);
+//     let span = document.createElement("span");
+//     span.innerHTML = "\u00d7";
+//     li.appendChild(span);
+//   }
+//   inputBox.value = "";
+//   saveData();
+// }
 
-listContainer.addEventListener("click", function (e) {
-  if (e.target.tagName === "LI") {
-    e.target.classList.toggle("checked");
-    saveData();
-  } else if (e.target.tagName == "SPAN") {
-    // We remove the parent element of span, which is LI itself
-    e.target.parentElement.remove();
-    saveData();
-  }
-});
+// listContainer.addEventListener("click", function (e) {
+//   if (e.target.tagName === "LI") {
+//     e.target.classList.toggle("checked");
+//     saveData();
+//   } else if (e.target.tagName == "SPAN") {
+//     // We remove the parent element of span, which is LI itself
+//     e.target.parentElement.remove();
+//     saveData();
+//   }
+// });
 
-function saveData() {
-  localStorage.setItem("data", listContainer.innerHTML);
-}
+// function saveData() {
+//   localStorage.setItem("data", listContainer.innerHTML);
+// }
 
-function showTask() {
-  listContainer.innerHTML = localStorage.getItem("data");
-}
+// function showTask() {
+//   listContainer.innerHTML = localStorage.getItem("data");
+// }
 
-showTask();
+// showTask();
