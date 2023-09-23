a533b2642c08fb7d5db1bae27baac2cab4e2edd5
Harun-Mac <iamharunjonuzi@gmail.com>
2023-09-23 18:43:12 +0200

    fix: readme update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-![Logo]()
-
 ![GitHub last commit (by committer)](https://img.shields.io/github/last-commit/harunjonuzi/App-Todoology)
 [![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-%23FE5196?logo=conventionalcommits&logoColor=white)](https://conventionalcommits.org)
 
@@ -17,7 +15,7 @@ Literally the best todolist application on my neighbourhood.
 
 ## Overview
 
-![Screenshot-1]()
+![Screenshot-1](app/utilities/images/screenshot1.png)
 
 ## Installation
 
@@ -25,19 +23,15 @@ Literally the best todolist application on my neighbourhood.
 
 ## Features
 
-- Feature #1
-- Feature #2
+- Add custom lists
+- Add tasks to custom lists
+- Clear completed tasks & delete whole lists
+- Remaining task counter
+- Local storage
 
 ## Contributing
-
 Contributions are always welcome!
-
 Please adhere to this project's `code of conduct`.
 
 ## License
-
-- Contributions are always welcome!
-
-- Please adhere to this project's `code of conduct`.
-
 - 📜 Some README files effectively engage and encourage contributions to a project, while other README files unexpectedly cast doubt on the very meaning of your existence, leaving you pondering the mysteries of life and questioning your choices in the vast cosmic scheme of things.
--- app/js/main.js
+++ app/js/main.js
@@ -1,108 +1,97 @@
-const listsContainer = document.querySelector("[data-lists]");
-const newListForm = document.querySelector("[data-new-list-form]");
-const newListInput = document.querySelector("[data-new-list-input]");
-const deleteListButton = document.querySelector("[data-delete-list-button]");
+const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-button]');
+const listDisplayContainer = document.querySelector(
+  '[data-list-display-container]'
+);
+const listTitleElement = document.querySelector('[data-list-title]');
+const listCountElement = document.querySelector('[data-list-count]');
+const tasksContainer = document.querySelector('[data-tasks]');
+const taskTemplate = document.getElementById('task-template');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const newTaskInput = document.querySelector('[data-new-task-input]');
+const clearCompletedTasksButton = document.querySelector(
+  '[data-clear-complete-tasks-button]'
+);
 
-const LOCAL_STORAGE_LIST_KEY = "task.lists";
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
-
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 
-////////////////////////////////////
-////////////////////////////////////
-// * Add classlist of active list from a function down below
-listsContainer.addEventListener("click", (e) => {
-  if (e.target.tagName.toLowerCase() === "li") {
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+// * Event listeners
+
+listsContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'li') {
     selectedListId = e.target.dataset.listId;
     saveAndRender();
   }
 });
 
-////////////////////////////////////
-////////////////////////////////////
+tasksContainer.addEventListener('click', (e) => {
+  if (e.target.tagName.toLowerCase() === 'input') {
+    const selectedList = lists.find((list) => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find(
+      (task) => task.id === e.target.id
+    );
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
+  }
+});
 
-////////////////////////////////////
-////////////////////////////////////
-
-////////////////////////////////////
-////////////////////////////////////
-
-////////////////////////////////////
-////////////////////////////////////
-// * Delete list function
-deleteListButton.addEventListener("click", (e) => {
-  // Create a new list with all the list items that don't contain a selectedListId
+deleteListButton.addEventListener('click', (e) => {
   lists = lists.filter((list) => list.id !== selectedListId);
   selectedListId = null;
   saveAndRender();
 });
 
-////////////////////////////////////
-////////////////////////////////////
-// * Add new categories
-newListForm.addEventListener("submit", (e) => {
+clearCompletedTasksButton.addEventListener('click', (e) => {
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks = selectedList.tasks.filter((task) => !task.complete);
+  saveAndRender();
+});
+
+newListForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  // 1.0 Get the value of the input
   const listName = newListInput.value;
-
-  // 2.0 Check if the input is empty
-  if (!listName) {
-    alert("You have to type something");
-    return;
-  }
-
-  // 3.0 Create a new list with the value of the input
+  if (listName == null || listName === '') return;
   const list = createList(listName);
   newListInput.value = null;
-
-  // 4.0 Push the new list to lists array
   lists.push(list);
   saveAndRender();
 });
 
+newTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName === '') return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+});
+
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+// * Functions
+
 function createList(name) {
-  return { id: Date.now().toString(), name: name, tasks: [] };
+  return {
+    id: Date.now().toString(),
+    name: name,
+    tasks: [],
+  };
 }
 
-////////////////////////////////////
-////////////////////////////////////
-// * Clear all lists and prepare to load new lists
-function render() {
-  clearElement(listsContainer);
-  lists.forEach((list) => {
-    // 1.0 Create list element
-    const listElement = document.createElement("li");
-
-    // 2.0 Add data-list-id to list element with an that we got from createList function
-    listElement.dataset.listId = list.id;
-
-    // 3.0 Add class to list element
-    listElement.classList.add("sidebar-list");
-
-    // 4.0 Add inner text to list element
-    listElement.innerText = list.name;
-
-    if (list.id === selectedListId) {
-      listElement.classList.add("active-list");
-    }
-    // 5.0 Append list element to listsContainer
-    listsContainer.appendChild(listElement);
-    // console.log(listElement);
-  });
-}
-render();
-
-function clearElement(element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-}
-
-function save() {
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
-  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+function createTask(name) {
+  return { id: Date.now().toString(), name: name, complete: false };
 }
 
 function saveAndRender() {
@@ -110,5 +99,69 @@ function saveAndRender() {
   render();
 }
 
-////////////////////////////////////
-////////////////////////////////////
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
+
+function render() {
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  clearElement(listsContainer);
+  renderLists();
+
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = 'none';
+  } else {
+    listDisplayContainer.style.display = '';
+
+    if (selectedList) {
+      listTitleElement.innerText = selectedList.name;
+    }
+
+    renderTaskCount(selectedList);
+    clearElement(tasksContainer);
+    renderTasks(selectedList);
+  }
+}
+
+function renderTasks(selectedList) {
+  selectedList.tasks.forEach((task) => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const checkbox = taskElement.querySelector('input');
+    checkbox.id = task.id;
+    checkbox.checked = task.complete;
+    const label = taskElement.querySelector('label');
+    label.htmlFor = task.id;
+    label.append(task.name);
+    tasksContainer.appendChild(taskElement);
+  });
+}
+
+function renderTaskCount(selectedList) {
+  const incompleteTaskCount = selectedList.tasks.filter(
+    (task) => !task.complete
+  ).length;
+  const taskString = incompleteTaskCount === 1 ? 'task' : 'tasks';
+  listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`;
+}
+
+function renderLists() {
+  lists.forEach((list) => {
+    const listElement = document.createElement('li');
+    listElement.dataset.listId = list.id;
+    listElement.classList.add('sidebar-list');
+    listElement.innerText = list.name;
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list');
+    }
+    listsContainer.appendChild(listElement);
+  });
+}
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+render();
